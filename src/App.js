f80ead195c29284e97d16be64c835ecc1163bc07
Özlem Hafalır <ozlemoztep@gmail.com>
2023-07-30 12:32:37 +0200

    Fetch categories into the app, update REAEDME

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
+import {useEffect, useState} from "react";
+import {axiosClient} from "./api/axiosDefaults";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome to Academy Site
-        </p>
-      </header>
-    </div>
-  );
+    const [categories, setCategories] = useState({ results: [] });
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const categories = await axiosClient.get('/categories');
+                setCategories(categories);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        handleMount();
+    }, []);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Welcome to Academy Site
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
